0820e223a6378cad712cc6dc5cfb8891afa852b7
layoliv <layoliveira96@gmail.com>
2023-08-07 10:17:31 -0300

    outside-click-assync

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import outsideClick from './modules/outside-click.js';
+import initMenuMobile from './modules/menu-mobile.js';
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -15,3 +16,4 @@ initModal();
 initTooltip();
 initDropdownMenu();
 outsideClick();
+initMenuMobile();
