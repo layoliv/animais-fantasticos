0fa0dc786f2aae750cc1317ab5bb410656c6ed47
layoliv <layoliveira96@gmail.com>
2023-08-07 15:01:42 -0300

    anima-num-js

---
+++ js/modules/anima-numeros.js
@@ -0,0 +1,20 @@
+export default function initAnimaNumeros() {
+
+}
+
+const numeros = document.querySelectorAll('[data-numero]');
+
+numeros.forEach((numero) => {
+  const total = +numero.innerText;
+  const incremento = Math.floor(total / 100);
+
+  let start = 0;
+  const timer = setInterval(() => {
+    start = start + incremento;
+    numero.innerHTML = start;
+    if(start > total) {
+      numero.innerText = total;
+      clearInterval(timer);
+    }      
+  }, 25 * Math.random());
+})
